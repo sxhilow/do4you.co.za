c712e71dfd3b636b7e3caf843188274e45b38cbd
Sahilo <160768468+sxhilow@users.noreply.github.com>
2025-12-19 12:28:56 +0200

    Initial commit

---
+++ src/data/layout.ts
@@ -0,0 +1,36 @@
+export const headerData = {
+  logo: {
+    // src: LogoBlack,
+    alt: "Client Logo",
+  },
+  navItems: [
+    { href: "/#", label: "Home" },
+    { href: "/#services", label: "Services" },
+    { href: "/#pricing", label: "Pricing" },
+  ],
+  cta: {
+    label: "Book a Call",
+    href: "#contact",
+    icon: {
+    //   src: CallIcon,
+      alt: "Call",
+    },
+    menuIcon: {
+    //   src: BurgerIcon,
+    },
+    closeIcon: {
+    //   src: CrossIcon,
+    },
+  },
+};
+
+
+export const footerData = {
+    logo: {
+    //   src: LogoWhite
+    alt: "Client Logo"
+    },
+    tagline: "Your Partner in Fast, Functional, Beautiful Websites",
+    copyright: "2025 Fayday.Studio. All rights reserved"
+}
+
