1aaa5ddf7bb2e9e959db6c38324c65e4c80e0232
Sahil Jada <jadasahil786@gmail.com>
2025-12-28 13:09:30 +0200

    feat: add astro sitemap package and integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from 'astro-sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://do4you.co.za",
+  integrations:[
+    sitemap()
+  ]
 });
